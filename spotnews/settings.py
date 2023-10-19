ef0ceb6de4aa684a9ccdebb21d0fd83c53da182b
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-10-18 22:00:55 -0300

    req 5

--- spotnews/settings.py
+++ spotnews/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = "spotnews.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR,'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 DATE_FORMAT = "d/m/Y"
 
 MEDIA_URL = "/img/"
-MEDIA_ROOT = BASE_DIR / "static"
+MEDIA_ROOT = BASE_DIR / "static/img"
 
 if "test" in sys.argv or "pytest" in sys.argv:
     MEDIA_URL = ""
--- spotnews/urls.py
+++ spotnews/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from news.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index, name="home-page"),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+        settings.STATIC_URL, document_root=settings.STATIC_URL
     )
